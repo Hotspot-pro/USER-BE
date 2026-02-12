df27ba5297d46b37d52b5ca923daa29ef2f4f4e7
HyeongseoShin <tlsgudtj123@gmail.com>
2026-02-12 16:50:41 +0900

    [HOTSPOT-46] feat: ApiResponse 추가

---
+++ src/main/java/hotspot/user/common/ApiResponse.java
@@ -0,0 +1,42 @@
+package hotspot.user.common;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import hotspot.user.common.exception.code.BaseErrorCode;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonPropertyOrder({"success", "code", "message", "data"})
+public class ApiResponse<T> {
+
+    private final boolean success;
+    private final String code;
+    private final String message;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private final T data;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(true, "200", "요청에 성공하였습니다.", data);
+    }
+
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return new ApiResponse<>(true, "200", message, data);
+    }
+
+    public static <T> ApiResponse<T> success() {
+        return new ApiResponse<>(true, "200", "요청에 성공하였습니다.", null);
+    }
+
+    public static <T> ApiResponse<T> fail(BaseErrorCode errorCode) {
+        return new ApiResponse<>(false, errorCode.getCustomCode(), errorCode.getMessage(), null);
+    }
+
+    public static <T> ApiResponse<T> fail(BaseErrorCode errorCode, String message) {
+        return new ApiResponse<>(false, errorCode.getCustomCode(), message, null);
+    }
+}
