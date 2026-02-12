1f93bbd91c2175ae1653f44681031478e1b8e715
HyeongseoShin <tlsgudtj123@gmail.com>
2026-02-12 16:40:30 +0900

    [HOTSPOT-46] feat: BaseEntity 추가

---
+++ src/main/java/hotspot/user/common/BaseEntity.java
@@ -0,0 +1,28 @@
+package hotspot.user.common;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_time", nullable = false, updatable = false)
+    private LocalDateTime createdTime;
+
+    @LastModifiedDate
+    @Column(name = "modified_time", nullable = false)
+    private LocalDateTime modifiedTime;
+}
